36f4f667859220c7169b22bbcab8176c18c7cbd1
DACAK197 <dacoffin@alaska.edu>
2018-04-01 18:21:07 -0800

    Fix plots

--- Brown.py
+++ Brown.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 read = np.genfromtxt('./Brown.dat', delimiter=', ', skiprows = 3)
 
 a,l,w,f = read[:,0], read[:,1], read[:,2], read[:,3]
-nca, ncl, ncw = 18, 7, 7 
+nca, ncl, ncw = 18, 9, 9 
 
 fig, axarr = plt.subplots(3,1, figsize=(14,3))
 
@@ -14,19 +14,19 @@ plt.subplot(131)
 plt.xlabel('Amplitude')
 plt.ylabel('L-shell')
 aw, lw, fw = a[nca+ncl:nca+ncl+ncw], l[nca+ncl:nca+ncl+ncw], f[nca+ncl:nca+ncl+ncw]
-#print(aw)
-X1, Y1 = np.mgrid[min(aw)-0.2:max(aw)+0.2:100j, min(lw)-0.2:max(lw)+0.2:100j]
+#print(lw)
+X1, Y1 = np.mgrid[min(aw):max(aw):100j, min(lw):max(lw):100j]
 interp1 = ip.griddata((aw, lw), fw, (X1, Y1), method='cubic')
-plt.imshow(interp1.T, extent=(min(aw)-1,max(aw)+1,min(lw)-1,max(lw)+1), origin='lower')
+plt.imshow(interp1.T, extent=(min(aw),max(aw),min(lw),max(lw)), origin='lower')
 
 plt.subplot(132)
 #plt.title('FWHM vs. amplitude')
 plt.xlabel('Amplitude')
 plt.ylabel('FWHM')   
 al, wl, fl = a[nca:nca+ncl], w[nca:nca+ncl], f[nca:nca+ncl]
-X2, Y2 = np.mgrid[min(al)-0.2:max(al)+0.2:100j, min(wl)-0.2:max(wl)+0.2:100j]
+X2, Y2 = np.mgrid[min(al):max(al):100j, min(wl):max(wl):100j]
 interp2 = ip.griddata((al, wl), fl, (X2, Y2), method='cubic')
-plt.imshow(interp2.T, extent=(min(al)-1,max(al)+1,min(wl)-1,max(wl)+1), origin='lower')
+plt.imshow(interp2.T, extent=(min(al),max(al),min(wl),max(wl)), origin='lower')
 
 plt.subplot(133)
 #plt.title('FWHM vs. L-shell')
@@ -34,9 +34,9 @@ plt.xlabel('L-shell')
 plt.ylabel('FWHM')   
 la, wa, fa = l[:nca], w[:nca], f[:nca]
 #print(la, wa, fa)
-X3, Y3 = np.mgrid[min(la)-0.2:max(la)+0.2:100j, min(wa)-0.2:max(wa)+0.2:100j]
+X3, Y3 = np.mgrid[min(la):max(la):100j, min(wa):max(wa)*1.1:100j]
 interp3 = ip.griddata((la, wa), fa, (X3, Y3), method='cubic')
-plt.imshow(interp3.T, extent=(min(la)-1,max(la)+1,min(wa)-1,max(wa)+1), origin='lower')
+plt.imshow(interp3.T, extent=(min(la),max(la),min(wa),max(wa)), origin='lower')
 
 plt.tight_layout()
 
